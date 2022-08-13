9768fcabc7ee0b918461238602f5cee414c86367
Kevin <kevin.desao@gmail.com>
2022-08-12 18:54:55 -0700

    feat: add lambda node code Add datatransformer index + Getaccountlist function + Uploadtos3 function

---
+++ lambda/dataTransformer/src/lib/uploadDataToS3.js
@@ -0,0 +1,29 @@
+const aws = require("aws-sdk");
+const { readFileSync } = require("node:fs");
+
+const uploadDataToS3 = async (sourceFileName) => {
+  const cloudGuardS3BuckedId = process.env.CLOUDGUARD_DATA_S3_BUCKET_ID;
+
+  const protectedAssetsData = readFileSync("/tmp/" + sourceFileName);
+
+  const s3Client = new aws.S3({ apiVersion: "2006-03-01" });
+
+  const params = {
+    Bucket: cloudGuardS3BuckedId,
+    Key: "processedData/" + sourceFileName,
+    Body: protectedAssetsData,
+  };
+
+  try {
+    const response = await s3Client.upload(params).promise();
+    console.log("Response: ", response);
+    return response;
+  } catch (err) {
+    console.log(err);
+    return {
+      message: "An error occure saving to S3",
+      error: JSON.stringify(err),
+    };
+  }
+};
+module.exports.uploadDataToS3 = uploadDataToS3;
